b71b7861fcac5b6452df816f5c710c15c65f5ef1
Evan Wu <evan.wu@authme.com>
2024-02-21 10:58:20 +0800

    temp

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,25 @@
 "use strict";
 
 var actions = require('@actions/core');
+var _require = require('googleapis'),
+  google = _require.google;
+var fs = require('fs');
+var glob = require('glob');
+var archiver = require('archiver');
 
 /** Google Service Account credentials  encoded in base64 */
 var credentials = actions.getInput('credentials', {
   required: false
 });
+var credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+var scopes = ['https://www.googleapis.com/auth/drive'];
+var auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
 try {
   // Get the input 'who-to-greet' from the workflow file
   var nameToGreet = actions.getInput('who-to-greet');
   console.log("Hello, ".concat(nameToGreet, "!"));
   console.log("Hello, ".concat(credentials, "!"));
+  console.log("Hello, ".concat(auth, "!"));
   actions.setOutput("greeting", "Hello, ".concat(nameToGreet, "!"));
 } catch (error) {
   actions.setFailed(error.message);
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const actions = require('@actions/core');
+const { google } = require('googleapis');
+const fs = require('fs');
+const glob = require('glob');
+const archiver = require('archiver');
 
 /** Google Service Account credentials  encoded in base64 */
 const credentials = actions.getInput('credentials', { required: false });
+
+const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+const scopes = ['https://www.googleapis.com/auth/drive'];
+const auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
 
 try {
   // Get the input 'who-to-greet' from the workflow file
   const nameToGreet = actions.getInput('who-to-greet');
   console.log(`Hello, ${nameToGreet}!`);
   console.log(`Hello, ${credentials}!`);
+  console.log(`Hello, ${auth}!`);
   actions.setOutput("greeting", `Hello, ${nameToGreet}!`);
 } catch (error) {
   actions.setFailed(error.message);
