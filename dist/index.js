749b32f54d3475c02a427cdf16de0a0969e48e29
Evan Wu <evan.wu@authme.com>
2024-02-21 10:47:51 +0800

    temp

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 
 var core = require('@actions/core');
+
+/** Google Service Account credentials  encoded in base64 */
+var credentials = actions.getInput('credentials', {
+  required: false
+});
 try {
   // Get the input 'who-to-greet' from the workflow file
   var nameToGreet = core.getInput('who-to-greet');
   console.log("Hello, ".concat(nameToGreet, "!"));
+  console.log("Hello, ".concat(credentials, "!"));
   core.setOutput("greeting", "Hello, ".concat(nameToGreet, "!"));
 } catch (error) {
   core.setFailed(error.message);
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const core = require('@actions/core');
 
+/** Google Service Account credentials  encoded in base64 */
+const credentials = actions.getInput('credentials', { required: false });
+
 try {
   // Get the input 'who-to-greet' from the workflow file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello, ${nameToGreet}!`);
+  console.log(`Hello, ${credentials}!`);
   core.setOutput("greeting", `Hello, ${nameToGreet}!`);
 } catch (error) {
   core.setFailed(error.message);
