c3871d88f1d13927cbc36fba4c45407eddacf57e
Evan Wu <evan.wu@authme.com>
2024-02-21 11:08:31 +0800

    temp

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,8 @@
 var actions = require('@actions/core');
 var _require = require('googleapis'),
   google = _require.google;
+var _require2 = require('google-auth-library'),
+  JWT = _require2.JWT;
 var fs = require('fs');
 var glob = require('glob');
 
@@ -11,6 +13,7 @@ var credentials = actions.getInput('credentials', {
   required: false
 });
 var credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+var scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file'];
 // const scopes = ['https://www.googleapis.com/auth/drive'];
 // const auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
 
@@ -19,7 +22,7 @@ try {
   var nameToGreet = actions.getInput('who-to-greet');
   console.log("Hello, ".concat(nameToGreet, "!"));
   console.log("Hello, ".concat(credentials, "!"));
-  console.log("Hello, ".concat(credentialsJSON, "!"));
+  // console.log(`Hello, ${credentialsJSON}!`);
   actions.setOutput("greeting", "Hello, ".concat(nameToGreet, "!"));
 } catch (error) {
   actions.setFailed(error.message);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const actions = require('@actions/core');
 const { google } = require('googleapis');
+const { JWT } = require('google-auth-library');
 const fs = require('fs');
 const glob = require('glob');
 
@@ -7,6 +8,7 @@ const glob = require('glob');
 const credentials = actions.getInput('credentials', { required: false });
 
 const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
+const scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file'];
 // const scopes = ['https://www.googleapis.com/auth/drive'];
 // const auth = new google.auth.JWT(credentialsJSON.client_email, null, credentialsJSON.private_key, scopes);
 
@@ -15,7 +17,7 @@ try {
   const nameToGreet = actions.getInput('who-to-greet');
   console.log(`Hello, ${nameToGreet}!`);
   console.log(`Hello, ${credentials}!`);
-  console.log(`Hello, ${credentialsJSON}!`);
+  // console.log(`Hello, ${credentialsJSON}!`);
   actions.setOutput("greeting", `Hello, ${nameToGreet}!`);
 } catch (error) {
   actions.setFailed(error.message);
